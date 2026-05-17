51d6482eb0c0df607433c347ab6854cfad81cb8d
Barry Chen <barrychen@live.com>
2026-05-17 16:18:47 +1000

    feat: initial scaffold of landing page. added basic components for button and card.

--- frontend/app/page.tsx
+++ frontend/app/page.tsx
@@ -1,65 +1,82 @@
-import Image from "next/image";
+import { Button } from "@/components/ui/button";
+import {
+  Card,
+  CardHeader,
+  CardTitle,
+  CardDescription,
+} from "@/components/ui/card";
 
 export default function Home() {
   return (
-    <div className="flex flex-col flex-1 items-center justify-center bg-zinc-50 font-sans dark:bg-black">
-      <main className="flex flex-1 w-full max-w-3xl flex-col items-center justify-between py-32 px-16 bg-white dark:bg-black sm:items-start">
-        <Image
-          className="dark:invert"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={100}
-          height={20}
-          priority
-        />
-        <div className="flex flex-col items-center gap-6 text-center sm:items-start sm:text-left">
-          <h1 className="max-w-xs text-3xl font-semibold leading-10 tracking-tight text-black dark:text-zinc-50">
-            To get started, edit the page.tsx file.
-          </h1>
-          <p className="max-w-md text-lg leading-8 text-zinc-600 dark:text-zinc-400">
-            Looking for a starting point or more instructions? Head over to{" "}
-            <a
-              href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-              className="font-medium text-zinc-950 dark:text-zinc-50"
-            >
-              Templates
-            </a>{" "}
-            or the{" "}
-            <a
-              href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-              className="font-medium text-zinc-950 dark:text-zinc-50"
-            >
-              Learning
-            </a>{" "}
-            center.
+    <div className="flex flex-col min-h-screen bg-zinc-50 font-sans dark:bg-black">
+      {/* 1. NAV BAR */}
+      <header className="flex justify-between border border-dashed border-red-500">
+        {/* a. Left: Logo */}
+        <div>Logo</div>
+
+        {/* b. Right - Action Buttons */}
+        <div className="border border-dashed border-red-500 flex">
+          <Button>Log In</Button>
+          <Button>Sign Up</Button>
+        </div>
+      </header>
+
+      {/* 2. MAIN */}
+      <main className="flex-1 bg-purple-300 ">
+        {/* a. Top: Hero Section */}
+        <section className="flex flex-col items-center justify-center text-center px-4 py-24 border border-dashed border-red-500">
+          <h1 className="text-5xl">Focus Deeply. Log effortlessly</h1>
+          <p className="text-xl max-w-xl">
+            A sleek and lightweight study tracker than pairs deep focus timers
+            with automated study logging to visualise your hours. Designed to
+            help you build better habits and conquer the term, one session at a
+            time.
           </p>
-        </div>
-        <div className="flex flex-col gap-4 text-base font-medium sm:flex-row">
-          <a
-            className="flex h-12 w-full items-center justify-center gap-2 rounded-full bg-foreground px-5 text-background transition-colors hover:bg-[#383838] dark:hover:bg-[#ccc] md:w-[158px]"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert"
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={16}
-              height={16}
-            />
-            Deploy Now
-          </a>
-          <a
-            className="flex h-12 w-full items-center justify-center rounded-full border border-solid border-black/[.08] px-5 transition-colors hover:border-transparent hover:bg-black/[.04] dark:border-white/[.145] dark:hover:bg-[#1a1a1a] md:w-[158px]"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Documentation
-          </a>
-        </div>
+          <Button>Create an account now</Button>
+        </section>
+
+        {/* b. Bottom: Features Section */}
+        <section className="border border-dashed border-red-500 max-w-6xl mx-auto">
+          <h2 className="text-3xl">FEATURES FEATURES</h2>
+          <div className="grid grid-cols-1 md:grid-cols-3 gap-6">
+            {/* [FEAT 1]*/}
+            <Card>
+              <CardHeader>
+                <CardTitle>⏱️ Customisable Trackers</CardTitle>
+                <CardDescription>
+                  Timer or stopwatch? Pick the right mode for the study session
+                  so you can lock in.
+                </CardDescription>
+              </CardHeader>
+            </Card>
+
+            {/* [FEAT 2]*/}
+            <Card>
+              <CardHeader>
+                <CardTitle>📊 Automated Logging</CardTitle>
+                <CardDescription>
+                  Your focus sessions are instantly logged against your tasks,
+                  subjects and terms
+                </CardDescription>
+              </CardHeader>
+            </Card>
+
+            {/* [FEAT 3]*/}
+            <Card>
+              <CardHeader>
+                <CardTitle>📆 Visual Your Progress</CardTitle>
+                <CardDescription>
+                  See how much work you have put in, day by day, week by week,
+                  term by term.
+                </CardDescription>
+              </CardHeader>
+            </Card>
+          </div>
+        </section>
       </main>
+
+      {/* 3. FOOTER */}
+      <footer className="flex bg-green-300">footer</footer>
     </div>
   );
 }
