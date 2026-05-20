1d9fe2d62884f8f5062edc7e567cf3f267b26104
MaskdDev <maskddev@gmail.com>
2026-05-20 22:34:48 +1000

    feat: Added user_courses table, and updated schema to throw 409 for POST /courses/:term when a course already exists

--- backend/db/migrations/20260520121721_termful_entities.sql
+++ backend/db/migrations/20260520121721_termful_entities.sql
@@ -1,9 +1,19 @@
 -- migrate:up
 
 -- Remove username fields from users table, as we're removing the username plugin.
+-- Add year and major columns to users table.
 alter table "users"
     drop column "username",
-    drop column "displayUsername";
+    drop column "displayUsername",
+    add column "year"  integer default null,
+    add column "major" text    default null;
+
+create table "user_courses"
+(
+    user_id     text not null references users (id),
+    course_name text not null,
+    primary key (user_id, course_name)
+)
 
 
 -- migrate:down
